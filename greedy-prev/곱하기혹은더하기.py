37da00b4b30061d7a7ab5841bb55a39ce6cd352c
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2023-07-25 13:06:40 +0900

    곱하기혹은더하기: solved

--- greedy-prev/곱하기혹은더하기.py
+++ greedy-prev/곱하기혹은더하기.py
@@ -1,1 +1,14 @@
-print("test")
+import sys
+input = sys.stdin.readline
+
+n = list(map(int, input().strip()))
+answer = n.pop(0)
+
+for i in n:
+    #print(i,n)
+    if answer*i>answer+i:
+        answer*=i
+    else:
+        answer+=i
+
+print(answer)
