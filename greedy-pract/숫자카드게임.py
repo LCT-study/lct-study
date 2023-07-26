17d1b92c153cc098713d2da8e0f478b4c30a7e09
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2023-07-26 14:29:37 +0900

    숫자카드게임: develope

--- greedy-pract/숫자카드게임.py
+++ greedy-pract/숫자카드게임.py
@@ -5,7 +5,8 @@ n,m = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(n)]
 answer = 0
 
-for i in arr:
-    if answer<min(i):
-        answer = min(i)
+for row in arr:
+    min_v = min(row)
+    answer = max(answer, min_v)
+    
 print(answer)
