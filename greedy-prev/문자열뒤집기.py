efc75e494118dbc02b128c2ee1a4aa0b7b9430f0
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2023-07-30 02:41:42 +0900

    문자열뒤집기: 1을 만들고자 뒤집어 지는 부분과 0을 만들고자 뒤집어 지는 부분을 동시에 카운트 가능하도록 함

--- greedy-prev/문자열뒤집기.py
+++ greedy-prev/문자열뒤집기.py
@@ -1,13 +1,27 @@
 import sys
 input = sys.stdin.readline
 
-n = input().strip()
-prev = n[0]
-count=0
+# 0001100
+# 1 
 
-for i in n[1:]:
-    if prev!=i:
-        count+=1
-    prev = i
-    
-print((count+1)//2)
+n= input().strip()
+# print(n)
+
+# 전부 0으로 바꾸는 경우 =  전부 1로 바꾸는 경우 = 0
+count0 = count1 = 0
+
+# 첫 원소 처리
+if n[0]=="1": 
+    count0+=1
+else:
+    count1+=1
+
+for i in range(len(n)-1):
+    if n[i]!=n[i+1]: #다음 원소가 현재 원소와 다른 경우
+        if n[i+1]=="1": # 다음 수가 1인 경우
+            count0+=1
+        else: # 다음 수가 0인 경우
+            count1+=1
+            
+print(min(count1, count0))
+        
