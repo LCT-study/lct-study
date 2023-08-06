c20dd3477e15ddc7da899fa2cd5e0eaed74b083d
YoonJoony <dbswns1101@naver.com>
2023-08-06 09:59:26 +0900

    push

--- 김윤준/구현/문자열재정렬.py
+++ 김윤준/구현/문자열재정렬.py
@@ -1,15 +1,16 @@
-String = input()
-Char = list()
-num = 0
+data = input()
 
-for i in String:
-    if i.isdigit(): # 숫자면 그 숫자를 num에 저장
-        num += int(i)
-    else:  # 아니면 문자니 Char에 문자 저장
-        Char.append(i)
+num = ['1','2','3','4','5','6','7','8','9']
+num_list = list()
+alpha_list = list()
 
-Char.sort()
-if num != 0: #숫자가 하나라도 존재하면
-    Char.append(str(num))
+for d in data:
+    if d in num:
+        num_list.append(int(d))
+    else:
+        alpha_list.append(d)
 
-print("".join(Char)) # 매개변수로 들어온 리스트에 있는 요소 하나하나 합쳐서 하나의 문자열로 바꿔줌
+alpha_list.sort()
+alpha_list.append(str(sum(num_list)))
+for a in alpha_list:
+    print(a,end='')
