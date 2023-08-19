cb3bebd9d6e6e0a5d277fd7a3a8237615867aad6
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2023-08-19 18:03:06 +0900

    [두배열의원소교체] clean code'

--- ayeon/sort/두배열의원소교체.py
+++ ayeon/sort/두배열의원소교체.py
@@ -6,20 +6,18 @@ input = sys.stdin.readline
 # 배열 a, 배열 b
 # 배열 a의 합이 최대
 
-n,m = map(int, input().split())
+n,k = map(int, input().split())
 arrA = list(map(int, input().split()))
 arrA.sort()
 arrB = list(map(int, input().split()))
 arrB.sort(reverse=True)
 
-idx, count = 0,0
-while m-count>0:
-    #print(idx, arrB[idx],arrA[idx])
-    if arrB[0]>arrA[0]:
-        arrB.append(arrA.pop(0))
-        arrA.append(arrB.pop(0))
-        #print(arrA, arrB)
-        count+=1
+for i in range(k):
+    if arrA[i]<arrB[i]:
+        arrA[i],arrB[i] = arrB[i],arrA[i]
+    else:
+        break
+    
 print(sum(arrA))
         
 """
